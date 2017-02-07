80849678d3e18f6eaa1aff33dd33aac054381076
Antony Partensky <apartensky@gmail.com>
2017-02-06 21:40:39 -0500

    Add Dockerfile and its dependencies to build a mev-web+rserve all-in-one container

---
+++ docker/mev/eval.R
@@ -0,0 +1,15 @@
+library (injectoR);
+library (topGO);
+library (RProtoBuf);
+library (WGCNA);
+library (jsonlite);
+shim ('jsonlite', callback = function (fromJSON, toJSON) {
+  define (toJson = function () function (x) toJSON (x, auto_unbox = TRUE));
+  define (fromJson = function () function (x) fromJSON (x));
+  define (result = function (toJson) function (x)
+    if (class (x) != 'try-error') {
+      r <- toJson (x);
+      class (r) <- 'result-json';
+      r;
+    } else x);
+}, binder = binder ());
