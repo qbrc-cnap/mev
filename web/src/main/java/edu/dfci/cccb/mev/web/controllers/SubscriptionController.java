22ac82810b047bb4faf2fa85ae0791218f599ba3
Antony Partensky <apartensky@gmail.com>
2017-03-14 14:27:31 -0400

    Subsctiber GET: allow to run on localhost

--- web/src/main/java/edu/dfci/cccb/mev/web/controllers/SubscriptionController.java
+++ web/src/main/java/edu/dfci/cccb/mev/web/controllers/SubscriptionController.java
@@ -1,25 +1,21 @@
 package edu.dfci.cccb.mev.web.controllers;
 
-import edu.dfci.cccb.mev.web.domain.Holder;
 import edu.dfci.cccb.mev.web.domain.Subscriber;
 import lombok.extern.log4j.Log4j;
 import org.springframework.context.annotation.Scope;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.social.google.api.Google;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.inject.Provider;
 import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
-import static org.springframework.web.bind.annotation.RequestMethod.*;
-import static org.springframework.http.HttpStatus.OK;
+
 import static java.util.Arrays.asList;
 
 /**
@@ -30,6 +26,7 @@ import static java.util.Arrays.asList;
 @RequestMapping("/subscriber")
 @Log4j
 public class SubscriptionController {
+    @Inject Environment environment;
     @Inject @Named ("subscriberEm" ) private EntityManager db;
     private @Inject Provider<Google> gPlus;
     private static final Set<String> ALLOWED = new HashSet<>(asList ("lev.v.kuznetsov@gmail.com", "apartensky@gmail.com"));
@@ -53,8 +50,11 @@ public class SubscriptionController {
 
     @RequestMapping (method = RequestMethod.GET)
     public Collection<Subscriber> subscribers () {
-       /* if (new HashSet<>(ALLOWED).removeAll(gPlus.get().plusOperations().getGoogleProfile().getEmailAddresses()))*/
+        log.debug("Subscriber GET env"+environment);
+        if (environment.getProperty("spring_profiles_active").contains("test")
+            || environment.getProperty("spring_profiles_active").contains("local")
+            || new HashSet<>(ALLOWED).removeAll(gPlus.get().plusOperations().getGoogleProfile().getEmailAddresses()))
             return db.createQuery("from " + Subscriber.class.getName()).getResultList();
-        /*else throw new IllegalStateException("YOU BEEN A BAD MONKEY");*/
+        else throw new IllegalStateException("YOU BEEN A BAD MONKEY");
     }
 }
