c73fb0d9b3929efd8b28b22540099e3459209bc1
Antony Partensky <apartensky@gmail.com>
2017-04-22 21:16:32 -0400

    Datasets View: add cccb-projects tab

--- web/src/main/javascript/edu/dfci/cccb/mev/web/ui/app/views/datasets/views.datasets.module.js
+++ web/src/main/javascript/edu/dfci/cccb/mev/web/ui/app/views/datasets/views.datasets.module.js
@@ -5,6 +5,7 @@ define(["mui",
 	"./_templates/views.datasets.google.tpl.html",
 	"./_templates/views.datasets.upload.tpl.html",
 	"./_templates/views.datasets.geods.tpl.html",
+	"./_templates/views.datasets.cccb.tpl.html",
 	"./_templates/views.datasets.imports.tpl.html",
 	"../../widgets/presets/widgets.presets.module",
 	"./session/views.datasets.session.module",
@@ -18,7 +19,7 @@ define(["mui",
 		'ng-grid',
 		'blob-util',
 		"geods"],
-function(ng, DatasetsVM, datasetsTemplate, tutorialsTemplate, googleTemplate, uploadTemplate, geodsTemplate, importsTemplate){
+function(ng, DatasetsVM, datasetsTemplate, tutorialsTemplate, googleTemplate, uploadTemplate, geodsTemplate, cccbTemplate, importsTemplate){
 	var module = ng.module("mui.views.datasets", ["Mev.GeodsModule","ngGrid"], arguments);
 	module.config(['$stateProvider', '$urlRouterProvider', "$$animateJsProvider",
 	     	function($stateProvider, $urlRouterProvider, $$animateJsProvider){
@@ -30,6 +31,29 @@ function(ng, DatasetsVM, datasetsTemplate, tutorialsTemplate, googleTemplate, up
 						displayName: "datasets",
 						template: datasetsTemplate,
 						redirectTo: "root.datasets.imports.upload",
+						controller: ["$scope", "cccbProjects", function(scope, cccbProjects){
+							function hasProjects(){
+								return _.isArray(cccbProjects) && cccbProjects.length>0
+							}
+							scope.vm = {
+								showCccbTab: hasProjects
+							}
+						}],
+						resolve: {
+							"cccbProjects": ["$http", function($http){
+								return $http({
+									method: 'GET',
+									url: '/cccb-projects',
+									params: {
+										format: "json"
+									}
+								}).then(function successCallback(response) {
+									return response.data;
+								}, function errorCallback(response) {
+									return [];
+								});
+							}]
+						},
 						data: {
 							headerUrl: "app/views/datasets/_templates/views.datasets.header.tpl.html"
 						},
@@ -78,6 +102,80 @@ function(ng, DatasetsVM, datasetsTemplate, tutorialsTemplate, googleTemplate, up
 						template: geodsTemplate,
 						displayName: "GEO"
 					})
+					.state("root.datasets.imports.cccb", {
+						url: "/cccb",
+						parent: "root.datasets.imports",
+						template: cccbTemplate,
+						templateUrl: "", 
+						displayName: "CCCB",
+						resolve: {
+							"projects": ["$http", function($http){
+								return $http({
+									method: 'GET',
+									url: '/cccb-projects',
+									params: {
+										format: "json"
+									}
+								}).then(function successCallback(response) {
+									return response.data;
+								}, function errorCallback(response) {
+									return [];
+								});
+							}]
+						},
+						controllerAs: "vm",
+						controller: ["$scope", "projects", "mevWorkspace", "$http", "$rootScope",
+							function(scope, cccbProjects, mevWorkspace, $http, $rootScope){
+							_.assign(this, {
+								projects: cccbProjects,
+								pagination: {
+									current: 1
+								},
+								getProjects: function(){
+									return scope.vm.projects;
+								},
+								import: function(project, file){
+
+									function doImport(payload){
+										if(!payload)
+											return;
+
+										return $http({
+											method: 'POST',
+											url: '/cccb-projects',
+											data: [payload]
+										})
+										.then(function(response){
+											$rootScope.$broadcast("mev:datasets:list:refreshed");
+										})
+										.catch(function(error){
+											alert("Could not import file:\n" + JSON.stringify(payload));
+										});
+									}
+									function renameIfExists(dataset){
+										if(dataset) {
+											var name = prompt("A dataset with this name already exists. Please specify a different name", file.name);
+											if (_.isNil(name) || _.isEmpty(name) || name === file.name)
+												return;
+											payload.files[0].name = name;
+											return payload;
+										}else{
+											return payload;
+										}
+									}
+									var payload  = _.cloneDeep(project);
+									payload.files=[_.cloneDeep(file)];
+									mevWorkspace.getDataset(file.name)
+										.then(renameIfExists)
+										.then(doImport);
+
+
+
+								}
+							});
+						}]
+					})
+
 					.state("root.datasets.imports.google", {
 						url: "/google",
 						parent: "root.datasets.imports",
